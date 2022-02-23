88963b15d7d982b0cede96b0e96832e19a1d2fdc
SNIPPIK <55327334+SNIPPIK@users.noreply.github.com>
2022-02-23 10:22:35 +0300

    Delete Modules directory

--- Modules/Functions/ConsoleLog.ts
+++
@@ -1,9 +0,0 @@
-import {W_Client} from "../../Core/Utils/W_Message";
-
-export default class ConsoleLog {
-    public readonly enable: boolean;
-    constructor() {
-        this.enable = true;
-    };
-    public run = (client: W_Client): (set: string) => void => client.console = (set: string): void => console.log(`[${(new Date).toLocaleString("ru")}] ${set}`);
-}
--- Modules/Music/src/Manager/Functions/StringTime.ts
+++
@@ -1,4 +0,0 @@
-//Добавляем в 0 к числу. Пример: 01:10
-export function StringTime(duration: string | number): string | number {
-    return (duration < 10) ? ('0' + duration) : duration;
-}
