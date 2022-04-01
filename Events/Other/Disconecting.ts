ba111906bab7f74880398107aaf5288c834221a5
SNIPPIK <55327334+SNIPPIK@users.noreply.github.com>
2022-04-01 18:44:11 +0300

    Delete Events directory

--- Events/Other/Disconecting.ts
+++
@@ -1,6 +0,0 @@
-export class shardDisconnect {
-    public readonly name: string = 'shardDisconnect';
-    public readonly enable: boolean = true;
-
-    public run = async (): Promise<void> => console.log(`[${(new Date).toLocaleString("ru")}] [WS]: Disconnecting...`);
-}
--- Events/Other/Reconecting.ts
+++
@@ -1,6 +0,0 @@
-export class shardReconnecting {
-    public readonly name: string = 'shardReconnecting';
-    public readonly enable: boolean = true;
-
-    public run = async (): Promise<void> => console.log(`[${(new Date).toLocaleString("ru")}] [WS]: Reconnecting...`);
-}
