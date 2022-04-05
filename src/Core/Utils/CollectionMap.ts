7d3e033e2dba661d6d3d244204a88617c03a0280
SNIPPIK <55327334+SNIPPIK@users.noreply.github.com>
2022-04-05 07:38:41 +0300

    Fixes CollectionMap.size

--- src/Core/Utils/CollectionMap.ts
+++ src/Core/Utils/CollectionMap.ts
@@ -1,15 +1,8 @@
 export class CollectionMap<K, V> extends Map<K, V> {
     public get Array(): V[] | null {
-        let db = [];
-        for (let value of Object.values(this)) db.push(value);
+        let db: V[] = [];
+        for (let [key, value] of Object.entries(this)) db.push(value);
 
         return db;
-    };
-
-    public get size(): number {
-        let size = Object.keys(this).length;
-
-        if (size > 0) return size;
-        else return Object.values(this).length
     };
 }
