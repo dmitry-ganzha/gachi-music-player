ab67c50602dd38e430042728799825f7ca76fa0b
SNIPPIK <55327334+SNIPPIK@users.noreply.github.com>
2022-06-03 09:17:02 +0300

    Update VoiceStateUpdate.ts

--- src/Events/Voice/VoiceStateUpdate.ts
+++ src/Events/Voice/VoiceStateUpdate.ts
@@ -6,23 +6,24 @@ export class voiceStateUpdate {
     public readonly name: string = 'voiceStateUpdate';
     public readonly enable: boolean = true;
 
-    public run = ({guild}: VoiceState, newState: VoiceState, client: WatKLOK): void => {
-        const queue: Queue = client.queue.get(guild.id);
+    public run = (oldState: VoiceState, newState: VoiceState, client: WatKLOK): void => {
+        const queue: Queue = client.queue.get(newState.guild.id);
 
         if (!queue) return;
 
         setImmediate(() => {
-            const voiceConnection: VoiceState[] = client.connections(guild); //Все пользователи в гс который нам надо
-            const FilterVoiceChannel: VoiceState[] = voiceConnection.filter((fn) => guild.members.cache.get(fn.id).user.bot); //Фильтруем пользователей чтоб боты не слушали музыку
+            const voiceConnection: VoiceState[] = client.connections(newState.guild); //Все пользователи в гс который нам надо
+            const FilterVoiceChannel: VoiceState[] = voiceConnection.filter((fn) => !newState.guild.members.cache.get(fn.id).user.bot); //Фильтруем пользователей чтоб боты не слушали музыку
+            const FindBotVoiceChannel = voiceConnection.find((fn: VoiceState) => fn.id === client.user.id); //Есть ли бот в гс
 
-            if (!voiceConnection.find((fn: VoiceState) => fn.id === client.user.id)) { //Если бота нет в гс
+            if (!FindBotVoiceChannel) { //Если бота нет в гс
                 queue.songs = [];
                 queue.options.stop = true;
                 return void queue.events.queue.emit('DestroyQueue', queue, queue.channels.message);
             }
 
             if (!FilterVoiceChannel) return void queue.events.helper.emit('StartQueueDestroy', queue); //Если нет слушателей, удаляем очередь
-            else if (queue.events.helper.DestroyStart) return void queue.events.helper.emit('CancelQueueDestroy', queue.player); //Если они есть, отмена удаления очереди
+            return void queue.events.helper.emit('CancelQueueDestroy', queue.player); //Если они есть, отмена удаления очереди
         });
     };
 }
