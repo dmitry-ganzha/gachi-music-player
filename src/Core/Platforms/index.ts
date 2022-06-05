1dc81d88f936300e51cefee1f001cd1697876d91
SNIPPIK <55327334+SNIPPIK@users.noreply.github.com>
2022-06-05 20:26:03 +0300

    Delete src/Core directory

--- src/Core/Platforms/index.ts
+++
@@ -1,6 +0,0 @@
-import {Spotify} from "./src/API/Spotify";
-import {VK} from "./src/API/VK";
-import {YouTube} from "./src/YouTube";
-import {SoundCloud} from "./src/API/SoundCloud";
-
-export {VK, Spotify, YouTube, SoundCloud};
