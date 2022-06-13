a3daa70d3e1a7b3a83339a3851f483dde033b0de
SNIPPIK <55327334+SNIPPIK@users.noreply.github.com>
2022-06-13 23:58:56 +0300

    Add files via upload

--- src/Core/Player/Structures/Message/Helper.ts
+++ src/Core/Player/Structures/Message/Helper.ts
@@ -5,11 +5,10 @@ const NotVer = "https://cdn.discordapp.com/attachments/860113484493881365/986005
 const NotFound = "https://cdn.discordapp.com/attachments/860113484493881365/986005794627670086/WTF.png";
 const NotImage = "https://cdn.discordapp.com/attachments/860113484493881365/940926476746883082/MusciNote.png";
 const Button = new ActionRowBuilder().addComponents([
-    new ButtonBuilder().setCustomId("last").setEmoji({id: "963787068264288346"}).setStyle(ButtonStyle.Secondary),
-    new ButtonBuilder().setCustomId("resume_pause").setEmoji({id: "963787098547171438"}).setStyle(ButtonStyle.Secondary),
-    new ButtonBuilder().setCustomId("skip").setEmoji({id: "963787083992940605"}).setStyle(ButtonStyle.Secondary),
-    new ButtonBuilder().setCustomId("replay").setEmoji({id: "963788841603457064"}).setStyle(ButtonStyle.Secondary)]
-)
-
+    new ButtonBuilder().setCustomId("last").setEmoji({id: "986009800867479572"}).setStyle(ButtonStyle.Secondary),
+    new ButtonBuilder().setCustomId("resume_pause").setEmoji({id: "986009725432893590"}).setStyle(ButtonStyle.Secondary),
+    new ButtonBuilder().setCustomId("skip").setEmoji({id: "986009774015520808"}).setStyle(ButtonStyle.Secondary),
+    new ButtonBuilder().setCustomId("replay").setEmoji({id: "986009690716667964"}).setStyle(ButtonStyle.Secondary)]
+);
 
 export {Ver, NotFound, NotVer, NotImage, Button};
