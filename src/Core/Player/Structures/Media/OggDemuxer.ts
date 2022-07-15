335e06189717c89be76a31c56e2853825be13544
SNIPPIK <55327334+SNIPPIK@users.noreply.github.com>
2022-07-16 00:32:34 +0300

    Исправление фризов в аудио

--- src/Core/Player/Structures/Media/OggDemuxer.ts
+++ src/Core/Player/Structures/Media/OggDemuxer.ts
@@ -14,45 +14,27 @@ type TransformDone = (error?: Error) => void;
 
 /**
  * @description Декодирует из ogg/opus в чистый opus
+ * @author prism-media
  */
 export class OggDemuxer extends Transform {
-    #_remainder: Buffer;
-    #_head: Buffer;
-    #_bitstream: number;
+    #_HeadSegment: boolean = null;
+    #_bitstream: number = null;
     public constructor(options = {}) {
         super(Object.assign({ readableObjectMode: true, ...options }));
-        this.#_remainder = null;
-        this.#_head = null;
-        this.#_bitstream = null;
     };
 
-    _destroy = (err: Error, cb: TransformDone) => {
-        if (cb) cb(err);
-
-        this?.destroy();
-        this.#_remainder = null;
-        this.#_head = null;
-        this.#_bitstream = null;
-    };
-
-    _transform = (chunk: Buffer, encoding: string, done: TransformDone) => {
-        //Если есть заготовленный фрагмент, то добавляем его к новому
-        if (this.#_remainder) {
-            chunk = Buffer.concat([this.#_remainder, chunk]);
-            this.#_remainder = null;
-        }
-
+    readonly _transform = (chunk: Buffer, encoding: string, done: TransformDone) => {
         //Если произойдет ошибка во время чтения потока завершим декодирование
         try {
             while (chunk) {
                 const result = this.#ReadBufferStream(chunk);
+
                 if (result) chunk = result;
                 else break;
             }
         } catch (error) {
             return done(error);
         }
-        this.#_remainder = chunk;
         done();
     };
 
@@ -61,7 +43,7 @@ export class OggDemuxer extends Transform {
      * @param chunk {Buffer} Фрагмент потока
      * @private
      */
-    #ReadBufferStream = (chunk: Buffer): false | Buffer => {
+    readonly #ReadBufferStream = (chunk: Buffer): false | Buffer => {
         if (chunk.length < OGG_PAGE_HEADER_SIZE) return false;
         if (!chunk.subarray(0, 4).equals(OGGs_HEADER)) throw Error(`Шаблон захвата не совпадает с ${OGGs_HEADER}`);
         if (chunk.readUInt8(4) !== STREAM_STRUCTURE_VERSION) throw Error(`Структура потока не совпадает с ${STREAM_STRUCTURE_VERSION}`);
@@ -73,7 +55,8 @@ export class OggDemuxer extends Transform {
         const table: Buffer = chunk.subarray(27, 27 + pageSegments); //Возвращает новый буфер, который ссылается на ту же память, что и оригинал, но смещен и обрезан по начальному и конечному индексам.
         const bitstream: number = chunk.readUInt32BE(14); //Считывает 32-битное целое число(14) из буфера
 
-        let sizes: Array<number> = [], totalSize = 0;
+        const sizes: Array<number> = [];
+        let totalSize = 0;
 
         //Собираем читаемые сегменты вместе
         for (let i = 0; i < pageSegments;) {
@@ -95,19 +78,19 @@ export class OggDemuxer extends Transform {
         let start = 27 + pageSegments;
 
         //Начинаем запись сегментов в текущий класс
-        for (const size of sizes) {
+        for (let size of sizes) {
             const segment: Buffer = chunk.subarray(start, start + size);
             const header: Buffer = segment.subarray(0, 8);
 
             //Если this._head не пустой
-            if (this.#_head) {
+            if (this.#_HeadSegment) {
                 if (header.equals(OPUS_TAGS)) this.emit('tags', segment);
                 else if (this.#_bitstream === bitstream) this.push(segment);
 
             //Если this._head пустой
             } else if (header.equals(OPUS_HEAD)) {
                 this.emit('head', segment);
-                this.#_head = segment;
+                this.#_HeadSegment = true;
                 this.#_bitstream = bitstream;
 
             //Если chunk невозможно опознать
