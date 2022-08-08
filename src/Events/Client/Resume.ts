f0d889e717fc418db7e3c84ff5289f34a85d2414
SNIPPIK <55327334+SNIPPIK@users.noreply.github.com>
2022-08-08 23:21:14 +0300

    Delete Resume.ts

--- src/Events/Client/Resume.ts
+++
@@ -1,8 +0,0 @@
-import {WatKLOK} from "../../Core/Client";
-
-export default class shardResume {
-    public readonly name: string = "shardResume";
-    public readonly enable: boolean = true;
-
-    public readonly run = (f1: null, f2: null, client: WatKLOK): void => void client.console("[Shard]: Resume...");
-}
