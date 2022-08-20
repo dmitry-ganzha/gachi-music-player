5929ea9b9a6c8062649b7b32eaa350cd1c95084f
SNIPPIK <55327334+SNIPPIK@users.noreply.github.com>
2022-08-20 21:17:52 +0300

    Delete src directory

--- src/Core/Platforms/index.ts
+++
@@ -1,6 +0,0 @@
-import { SoundCloud } from "./src/API/SoundCloud";
-import { Spotify } from "./src/API/Spotify";
-import { VK } from "./src/API/VK";
-import {YouTube} from "./src/YouTube";
-
-export {YouTube, Spotify, SoundCloud, VK};
--- src/Events/Client/Reconecting.ts
+++
@@ -1,9 +0,0 @@
-import {WatKLOK} from "../../Core/Client/Client";
-import {Event} from "../../Structures/Event";
-
-export default class shardReconnecting extends Event<null, null> {
-    public readonly name: string = "shardReconnecting";
-    public readonly enable: boolean = true;
-
-    public readonly run = (f1: null, f2: null, client: WatKLOK): void => void client.console("[WS]: Reconnecting...");
-}
