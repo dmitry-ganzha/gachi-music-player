247964020348c26d55a863a2dba014d8e73856b1
SNIPPIK <55327334+SNIPPIK@users.noreply.github.com>
2022-08-20 20:45:53 +0300

    Delete src directory

--- src/Events/Client/Disconecting.ts
+++
@@ -1,8 +0,0 @@
-import {WatKLOK} from "../../Core/Client";
-
-export default class shardDisconnect {
-    public readonly name: string = "shardDisconnect";
-    public readonly enable: boolean = true;
-
-    public readonly run = (f1: null, f2: null, client: WatKLOK): void => void client.console("[WS]: Disconnecting...");
-}
