c66b200c1c628b81ca8999c230ab6cf8d85b3df9
SNIPPIK <55327334+SNIPPIK@users.noreply.github.com>
2022-08-26 11:17:27 +0300

    Delete src directory

--- src/Structures/Event.ts
+++
@@ -1,8 +0,0 @@
-import {WatKLOK} from "../Core/Client/Client";
-
-export class Event<P1, P2> {
-    public readonly name: string = "undefined";
-    public readonly enable: boolean = false;
-
-    public readonly run: (f: P1, f2: P2, client: WatKLOK) => void;
-}
--- src/Structures/Module.ts
+++
@@ -1,7 +0,0 @@
-import {WatKLOK} from "../Core/Client/Client";
-
-export class Module {
-    public readonly enable: boolean = true;
-
-    public run: (client: WatKLOK) => any;
-}
