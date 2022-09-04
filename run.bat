56e673245f079c487148dd129d0645ef5c4e3740
SNIPPIK <55327334+SNIPPIK@users.noreply.github.com>
2022-09-04 18:33:08 +0300

    Add files via upload

---
+++ run.bat
@@ -0,0 +1,29 @@
+@Echo off
+
+:menu
+cls
+Echo Choice number
+Echo 1 - run Client
+Echo 2 - run ShardManager
+Echo 3 - run Builder
+
+echo.
+Set /p choice="Num: "
+
+if "%choice%"=="1" (
+    echo running Client...
+    cd _Build
+    node ./src/Core/Client/Client.js
+) else if "%choice%"=="2" (
+    echo running ShardManager...
+	cd _Build
+	node ./src/Core/Client/ShardManager.js
+) else if "%choice%"=="3"  (
+	echo running builder...
+	tsc -p ./tsconfig.json
+) else (
+    echo %choice% is not 1-3
+)
+
+goto menu
+pause >nul
