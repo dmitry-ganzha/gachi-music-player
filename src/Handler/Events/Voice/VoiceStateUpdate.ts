f55871ef3f6afe6c76c17426ac70bd1dd5fd7601
SNIPPIK <55327334+SNIPPIK@users.noreply.github.com>
2022-09-25 18:56:04 +0300

    Add files via upload

--- src/Handler/Events/Voice/VoiceStateUpdate.ts
+++ src/Handler/Events/Voice/VoiceStateUpdate.ts
@@ -13,14 +13,26 @@ export class voiceStateUpdate extends Event<VoiceState, VoiceState> {
         const queue: Queue = client.queue.get(newState.guild.id); //Очередь сервера
         const Guild = oldState.guild;
         const ChannelID = oldState?.channel?.id || newState?.channel?.id;
+        const filter = (member: GuildMember) => this.#filter(member, ChannelID);
 
         setImmediate(() => {
-            const voice = getVoiceConnection(Guild.id);
-            const usersSize = newState.channel?.members?.filter(member => this.#filter(member, ChannelID))?.size ?? 0;
+            const voice = getVoiceConnection(Guild.id), isBotVoice = !!oldState.channel?.members?.find((member => member.user.id === client.user.id));
+            const usersSize = newState.channel?.members?.filter(filter)?.size ?? oldState.channel?.members?.filter(filter)?.size;
 
-            //Нечего не делаем, если есть хоть один пользователь
-            if (usersSize > 0) return;
+            //Если есть голосовое подключение и нет пользователей
+            if (isBotVoice && voice && usersSize < 1) Voice.Disconnect(Guild);
 
+            //Если нет очереди, то ничего не делаем
+            if (!queue) return;
+
+            //Бот не подключен к голосовому каналу
+            if (!isBotVoice) return queue.TimeDestroying("start");
+
+            //Если пользователей нет в голосовом канале
+            if (usersSize > 0) queue.TimeDestroying("cancel"); //Если есть очередь сервера, отмена удаления!
+            else if (usersSize < 1) queue.TimeDestroying("start"); //Если есть очередь сервера, удаляем!
+
+            /*
             //Если есть голосовое подключение и нет пользователей
             if (voice && usersSize === 0) Voice.Disconnect(Guild);
 
@@ -33,6 +45,7 @@ export class voiceStateUpdate extends Event<VoiceState, VoiceState> {
             //Если пользователей нет в голосовом канале
             if (usersSize > 0) queue.TimeDestroying("cancel"); //Если есть очередь сервера, отмена удаления!
             else if (usersSize < 1) queue.TimeDestroying("start"); //Если есть очередь сервера, удаляем!
+             */
         });
     };
     //Фильтруем пользователей в голосовом канале
